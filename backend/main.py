dcf6917915d4ca1ebe6c9c03483d3613e5b17c56
Gabriel Nogueira <gabrielgoncalvesnogueira@gmail.com>
2022-08-01 00:39:46 -0300

    backend

---
+++ backend/main.py
@@ -0,0 +1,45 @@
+from typing import List
+import fastapi as _fastapi
+import fastapi.security as _security
+from fastapi.middleware.cors import CORSMiddleware
+
+import sqlalchemy.orm as _orm
+import services as _services, schemas as _schemas
+
+app = _fastapi.FastAPI()
+
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+_services.create_database()
+
+@app.post("/api/users")
+async def create_user(user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    db_user = await _services.get_user_by_email(user.email, db)
+    if db_user:
+        raise _fastapi.HTTPException(status_code=400, detail="User with this email already exists in the database")
+    
+    user = await _services.create_user(user, db)
+
+    return await _services.create_token(user)
+
+@app.post("/api/token")
+async def generate_token(form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(), db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    user = await _services.authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise _fastapi.HTTPException(status_code=401, detail="Incorrect email or password")
+    return await _services.create_token(user)
+
+@app.get("/api/users/me", response_model=_schemas.User)
+async def get_user(user: _schemas.User = _fastapi.Depends(_services.get_current_user)):
+    return user
